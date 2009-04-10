c4be5c8ab1f787e66d00b84eeab7053247f13b5a
edd (Dirk Eddelbuettel) <edd@debian.org>
2009-04-10 19:28:34 +0000

    add simple businessDay function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rquantlib@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/calendars.R
@@ -0,0 +1,33 @@
+## RQuantLib -- R interface to the QuantLib libraries
+##
+## Copyright (C) 2002 - 2009 Dirk Eddelbuettel <edd@debian.org>
+##
+## $Id$
+##
+## This file is part of the RQuantLib library for GNU R.
+## It is made available under the terms of the GNU General Public
+## License, version 2, or at your option, any later version,
+## incorporated herein by reference.
+##
+## This program is distributed in the hope that it will be
+## useful, but WITHOUT ANY WARRANTY; without even the implied
+## warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+## PURPOSE.  See the GNU General Public License for more
+## details.
+##
+## You should have received a copy of the GNU General Public
+## License along with this program; if not, write to the Free
+## Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+## MA 02111-1307, USA
+
+businessDay <- function(calendar, dates) {
+    stopifnot(is.character(calendar))
+    stopifnot(class(dates)=="Date")
+    val <- .Call("QL_isBusinessDay",
+                 list(calendar=calendar),
+		 dates,
+                 PACKAGE="RQuantLib")
+    val <- as.logical(val[[1]])
+    names(val) <- dates
+    val
+}
