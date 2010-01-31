6e224c7b9a6bcfa8fb07a442dc885806b2e57349
knguyen (Khanh Nguyen) <nguyen.h.khanh@gmail.com>
2010-01-31 19:37:39 +0000

    Matlab bkput, bkcall for option on bond
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rquantlib@168 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/MFds.R
+++ pkg/R/MFds.R
@@ -1,3 +1,134 @@
+mf.bkcall <- function(Strike, ZeroData, Sigma, BondData, Settle, 
+Expiry, Period=2, Basis=6, EndMonthRule=1, InterpMethod='linear', 
+StrikeConvention=0) {
+
+  CouponRate <- BondData[,2]
+  CleanPrice <- BondData[,1]
+  Maturity <- BondData[,3]
+  Face <- BondData[,4]
+
+
+  
+  cf <- mf.cfamounts(CouponRate, Settle, Maturity, Period, Basis, Face=Face)
+  accruedInterest <- -cf[1,2]
+  cfAmount <- cf[-1,2]
+  cfDate <- cf[-1,1]
+
+  ZeroDates <- ZeroData[,1]
+  ZeroRates <- ZeroData[,2]  
+  Frequency <- 0
+
+  if (ncol(ZeroData)==2) Frequency <- rep(2, length(ZeroDates))
+  else Frequency <- ZeroData[,3]
+  
+  for (i in 1:length(ZeroDates)){   
+    if (Frequency[i] > 0){
+      ZeroRates[i] <- Frequency[i] * log(1 + ZeroRates[i] / Frequency[i])
+    }
+  }
+  
+  if (Expiry > tail(ZeroDates,1)){
+    ZeroDates <- c(Settle, ZeroDates, Expiry)
+    ZeroRates <- c(ZeroRates[1], ZeroRates, tail(ZeroRates,1))
+  } else {
+    ZeroDates <- c(Settle, ZeroDates)
+    ZeroRates <- c(ZeroRates[1], ZeroRates)
+  }
+  
+  FwdPrice <- 0
+  zT <- 0
+  T <- 0
+  relevantDates <- cfDate[cfDate < Expiry]
+  I <- 0
+  f <- approxfun(as.numeric(ZeroDates), ZeroRates, method=InterpMethod)
+  if (length(relevantDates)==0) {
+    I <- 0
+  } else {
+    relevantCF <- cfAmount[1:length(relevantDates)]
+    TimeToCFs <- RQuantLib::yearFraction(rep(Settle, length(relevantDates)),
+                                         relevantDates, as.numeric(rep(Basis, length(relevantDates))))
+    z <- f(as.numeric(relevantDates))
+    I <- sum(relevantCF * exp(-z * TimeToCFs))
+  }                          
+  
+  T <- RQuantLib::yearFraction(Settle, Expiry, Basis)
+  zT <- f(Expiry)
+  DirtyPrice <- CleanPrice + accruedInterest
+  FwdPrice <- (DirtyPrice - I) * exp(zT * T)
+  P <- exp(-0.1 * T)
+  
+  d1 <- (log(FwdPrice / Strike) + Sigma^2 * T /2) / (Sigma * sqrt(T))
+  d2 <- d1 - Sigma * sqrt(T)
+  
+  P * (FwdPrice * pnorm(d1) - Strike * pnorm(d2))                                         
+
+}
+
+
+mf.bkput <- function(Strike, ZeroData, Sigma, BondData, Settle, 
+Expiry, Period=2, Basis=6, EndMonthRule=1, InterpMethod='linear', 
+StrikeConvention=0) {
+
+  CouponRate <- BondData[,2]
+  CleanPrice <- BondData[,1]
+  Maturity <- BondData[,3]
+  Face <- BondData[,4]
+  
+  cf <- mf.cfamounts(CouponRate, Settle, Maturity, Period, Basis, Face=Face)
+  accruedInterest <- -cf[1,2]
+  cfAmount <- cf[-1,2]
+  cfDate <- cf[-1,1]
+
+  ZeroDates <- ZeroData[,1]
+  ZeroRates <- ZeroData[,2]
+  Frequency <- 0
+
+  if (ncol(ZeroData)==2) Frequency <- rep(2, length(ZeroDates))  
+  else Frequency <- ZeroData[,3]
+  
+  for (i in 1:length(ZeroDates)){   
+    if (Frequency[i] > 0){
+      ZeroRates[i] <- Frequency[i] * log(1 + ZeroRates[i] / Frequency[i])
+    }
+  }
+  
+  if (Expiry > tail(ZeroDates,1)){
+    ZeroDates <- c(Settle, ZeroDates, Expiry)
+    ZeroRates <- c(ZeroRates[1], ZeroRates, tail(ZeroRates,1))
+  } else {
+    ZeroDates <- c(Settle, ZeroDates)
+    ZeroRates <- c(ZeroRates[1], ZeroRates)
+  }
+  
+  FwdPrice <- 0
+  zT <- 0
+  T <- 0
+  relevantDates <- cfDate[cfDate < Expiry]
+  I <- 0
+  f <- approxfun(as.numeric(ZeroDates), ZeroRates, method=InterpMethod)
+  if (length(relevantDates)==0) {
+    I <- 0
+  } else {
+    relevantCF <- cfAmount[1:length(relevantDates)]
+    TimeToCFs <- RQuantLib::yearFraction(rep(Settle, length(relevantDates)),
+                                         relevantDates, as.numeric(rep(Basis, length(relevantDates))))
+    z <- f(as.numeric(relevantDates))
+    I <- sum(relevantCF * exp(-z * TimeToCFs))
+  }                          
+  
+  T <- RQuantLib::yearFraction(Settle, Expiry, Basis)
+  zT <- f(Expiry)
+  DirtyPrice <- CleanPrice + accruedInterest
+  FwdPrice <- (DirtyPrice - I) * exp(zT * T)
+  P <- exp(-0.1 * T)
+  
+  d1 <- (log(FwdPrice / Strike) + Sigma^2 * T /2) / (Sigma * sqrt(T))
+  d2 <- d1 - Sigma * sqrt(T)
+  
+
+  P *(Strike * pnorm(-d2) - FwdPrice * pnorm(-d1))    
+}
+
 bkcaplet.once <- function(x){
 
   CRate <- x[1]
---
+++ pkg/src/MFcoupon.cpp
@@ -0,0 +1,65 @@
+#include "rquantlib.hpp"
+
+
+RcppExport SEXP cfamounts(SEXP params){
+       
+    SEXP rl=R_NilValue;
+    char* exceptionMesg=NULL;
+    try{
+        RcppParams rparam(params); 
+
+        QuantLib::Date maturity(dateFromR(rparam.getDateValue("Maturity")));
+        QuantLib::Date settle(dateFromR(rparam.getDateValue("Settle")));
+        QuantLib::Date issue(dateFromR(rparam.getDateValue("IssueDate")));
+
+        double rate = rparam.getDoubleValue("CouponRate");
+        std::vector<double> rateVec(1, rate);
+        double faceAmount = rparam.getDoubleValue("Face");
+        double period = rparam.getDoubleValue("Period");
+        double basis = rparam.getDoubleValue("Basis");
+        DayCounter dayCounter = getDayCounter(basis);
+        Frequency freq = getFrequency(period);
+        Period p(freq);
+        double EMR = rparam.getDoubleValue("EMR");
+        Calendar calendar=UnitedStates(UnitedStates::GovernmentBond);
+        
+        
+        Schedule sch(settle, maturity, p, calendar, 
+                     Unadjusted, Unadjusted, DateGeneration::Backward, 
+                     (EMR == 1)? true : false);
+
+        FixedRateBond bond(1, faceAmount, sch, rateVec, dayCounter, Following,
+                           100, issue);
+
+        //cashflow
+        int numCol = 2;
+        std::vector<std::string> colNames(numCol);
+        colNames[0] = "Date";
+        colNames[1] = "Amount";
+        RcppFrame frame(colNames);
+        
+        Leg bondCashFlow = bond.cashflows();
+        for (unsigned int i = 0; i< bondCashFlow.size(); i++){
+            std::vector<ColDatum> row(numCol);
+            Date d = bondCashFlow[i]->date();
+            row[0].setDateValue(RcppDate(d.month(), d.dayOfMonth(), d.year()));
+            row[1].setDoubleValue(bondCashFlow[i]->amount());
+            frame.addRow(row);
+        }
+                     
+        RcppResultSet rs;
+        rs.add("cashFlow", frame);
+        rl = rs.getReturnList();
+
+    } catch(std::exception& ex) {
+        exceptionMesg = copyMessageToR(ex.what());
+    } catch(...) {
+        exceptionMesg = copyMessageToR("unknown reason");
+    }   
+    if(exceptionMesg != NULL)
+        Rf_error(exceptionMesg);    
+    return rl;
+}
+
+
+
